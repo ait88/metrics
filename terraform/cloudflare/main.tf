010f05f265534e00eff693e86aa5e4573abab91d
ait88 <74575336+ait88@users.noreply.github.com>
2025-06-01 14:22:24 +0930

    cleanup

--- terraform/cloudflare/main.tf
+++ terraform/cloudflare/main.tf
@@ -57,3 +57,29 @@ resource "cloudflare_record" "wireguard" {
   ttl     = 1
   proxied = false # Don't proxy VPN traffic
 }
+
+# Create a Page Rule to enable specific caching for static content
+#resource "cloudflare_page_rule" "grafana_caching" {
+#  zone_id  = var.cloudflare_zone_id
+#  target   = "grafana.${var.subdomain_prefix}.${var.domain_name}/public/*"
+#  priority = 1
+#
+#  actions {
+#    cache_level = "cache_everything"
+#    edge_cache_ttl = 43200 # 12 hours
+#  }
+#}
+#
+# Optional: Create a Firewall rule to block malicious traffic
+#resource "cloudflare_firewall_rule" "block_bad_bots" {
+#  zone_id     = var.cloudflare_zone_id
+#  description = "Block bad bots"
+#  filter_id   = cloudflare_filter.bad_bots.id
+#  action      = "block"
+#}
+#
+#resource "cloudflare_filter" "bad_bots" {
+#  zone_id     = var.cloudflare_zone_id
+#  description = "Bad bots filter"
+#  expression  = "(http.user_agent contains \"Baiduspider\") or (http.user_agent contains \"Yandex\")"
+#}
